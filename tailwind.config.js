7c2ec9f1afa52e9b840d309ab542c0bada04e631
tsabith <abubakartsabitghufron@gmail.com>
2025-01-15 04:03:53 +0700

    lengkapi

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
+        primary: '#3da6cb',
         secondary: '#64748b',
         dark: '#0f172a',
       },
